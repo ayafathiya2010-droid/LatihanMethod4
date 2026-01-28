035813f09834c61084a3e2b5899db38f9bc503e5
ayafathiya2010-droid <ayafathiya2010@gmail.com>
2026-01-28 13:55:58 +0700

    method 4

--- LatihanMethod4/Program.cs
+++ LatihanMethod4/Program.cs
@@ -1,1 +1,11 @@
-﻿
+﻿using System.ComponentModel.Design;
+
+int health = 100;
+
+void takedamage(int damage)
+{
+    health = health - damage;
+}
+
+takedamage(5);
+Console.WriteLine($"Current health: {health}"
